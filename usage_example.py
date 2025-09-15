d8aca8a2ca756d6a23217e29a7106f8f232f5176
David Noack <ddnoack@gmail.com>
2025-09-15 19:01:34 +0200

    adjust usage_example.py to work on normal machines by applying filters to parquet before loading data

--- usage_example.py
+++ usage_example.py
@@ -1,38 +1,42 @@
-### usage_example
-
-"""This is an example showing how GROW can be easily subset for specific needs. In this example, we might want to investigate
-the seasonal pattern of groundwater observations in Brazil. For that, we need to 1.) extract monthly groundwater time series from Brazil,
-2.) aggregate the time series and 3.) adjust the units."""
+"""This example shows how to apply filters to query GROW even on machines that do not have enough RAM to load the whole dataset at once.
+Let's assume we want to investigate the seasonal pattern of groundwater observations in Brazil.
+For that, we need to
+  1.) extract monthly groundwater time series from Brazil,
+  2.) aggregate the time series and
+  3.) adjust the units.
+"""
 
 import pandas as pd  # imported version: 2.2.3
 
 # load GROW tables
-attributes = pd.read_csv(
-    "/mnt/storage/grow/final_grow/grow_attributes.csv", sep=";"
-)  # table in csv-format
-timeseries = pd.read_parquet(
-    "/mnt/storage/grow/final_grow/grow_timeseries.parquet"
-)  # table in parquet-format
+print("loading csv...")
+attributes = pd.read_csv("./data/grow_attributes.csv", sep=";")
 
-# 1.) subset attribute and time series table
+print("filtering...")
+# 1.) Select GROW_IDs based on filters
 # only time series in Brazil ("BRA")
 # only time series which are monthly or daily (not yearly; YS)
 attr_bra_monthly = attributes[
     (attributes.country == "BRA") & (attributes.interval != "YS")
-]  # subset attribute table by data flag columns
-ts_bra = timeseries[
-    timeseries.GROW_ID.isin(attr_bra_monthly.GROW_ID)
-]  # subset time series table BY GROW_ID
+]
+
+
+print("loading parquet with filter...")
+timeseries = pd.read_parquet(
+    "./data/grow_timeseries.parquet",
+    filters=[("GROW_ID", "in", attr_bra_monthly["GROW_ID"])],
+)
 
 # 2.) aggregate time series to a monthly resolution
-ts_bra.drop(
+# drop filter columns that are not needed
+timeseries.drop(
     columns=["date", "interval", "year", "aggregated_from_n_values", "plateaus"],
     inplace=True,
-)  # drop filter columns that are not needed
-ts_bra_monthly = ts_bra.groupby(
-    ["GROW_ID", "month"], as_index=False
-).mean()  # aggregate all numeric variables per ID and month
+)
+# aggregate all numeric variables per ID and month
+ts_bra_monthly = timeseries.groupby(["GROW_ID", "month"], as_index=False).mean()
 
+print("processing...")
 # 3.) adjust units
 """All quantity units are given in mm/year. The unit can be adjusted to the daily or monthly scale by the division of 365 or 12. 
 All variables with quantity units are arranged in a row (here column 8 to 13)"""
@@ -44,4 +48,7 @@ ts_bra_monthly["month"] = pd.to_datetime(
 )  # convert month-column to datetime
 
 """Now, you are ready to start your analysis :)"""
-print()
+print(
+    f"filtered {ts_bra_monthly['GROW_ID'].size} timeseries with a total of {ts_bra_monthly.size} data points"
+)
+print(ts_bra_monthly.head())
